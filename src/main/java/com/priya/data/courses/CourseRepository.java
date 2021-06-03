c84d826aa552441a86fe06a5c68a7ab40e21777d
Priyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2021-06-03 16:57:49 +0530

    First Commit

---
+++ src/main/java/com/priya/data/courses/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.priya.data.courses;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+//public List<Course> findByName(String name);
+	public List<Course> findByTopicId(String topicId);
+}
